8b7aa9350922f1d2c2b34c4339e7da3f10ae48df
sijia_server <1353512365@qq.com>
2026-09-20 17:08:54 +0800

    s

--- gemm_hopper_tma_ws2.cu
+++ gemm_hopper_tma_ws2.cu
@@ -17,7 +17,7 @@
 #include <cstring>
 #include <vector>
 constexpr int M = 4096, N = 4096, K = 4096;
-constexpr int WM = 64, WN = 64, WK = 16, BK = 64, STAGES = 4;
+constexpr int WM = 64, WN = 64, WK = 16, BK = 64, STAGES = 3;
 constexpr int WARP_GROUP_THREADS = 128;
 constexpr int BLOCK_THREADS = 160; // 160threads ; 128 threads for WGMMA, 32 threads for TMA async proxy
 constexpr int FIRST_PRODUCER = 128;
